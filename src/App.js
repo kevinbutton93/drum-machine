a7ef7b67199514362041eed6ec454ff44eec932e
Kevin Button <kevin.button93@gmail.com>
2021-04-10 13:52:38 +0100

    litle test using hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 
 function App() {
+  const [count, setCount] = useState(0)
+
+
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Counter Test</h1>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(count + 1) }>Increment</button>
+    </div>
   );
 }
 
 export default App;
+ 
